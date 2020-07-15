172147f09c1c9dc15f854e74bea1380af4040e4d
osamamousa204 <osamamousa204@gmail.com>
2020-07-15 05:21:55 +0300

    initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './header.scss';
+function Header (){
+    return (
+        <Navbar bg="light" variant="light">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home" >Front Store</Nav.Link>
+          <Nav.Link href="#pricing">CART</Nav.Link>
+        </Nav>
+      </Navbar>
+    )
+}
+export default Header;
